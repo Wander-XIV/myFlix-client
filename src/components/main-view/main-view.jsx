ada4c5b8405bb9719e59db3dc3b39f8a4428a9d1
Wander-XIV <aroy1420@gmail.com>
2022-06-05 19:25:08 -0400

    updates
    Updated existing cards and added login and registration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,52 +1,100 @@
 import React from 'react';
 import axios from 'axios';
 
+//adding components to the main-view
+
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+//getting array of movies from remote and displaying as a list
 export class MainView extends React.Component {
-
   constructor() {
     super();
+    //initial state for main-view
     this.state = {
       movies: [],
-      selectedMovie: null
-    }
+      selectedMovie: null,
+      registered: null,
+      user: null,
+    };
   }
-
-  componentDidMount(){
-    axios.get('https://myflix-db14.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
+  componentDidMount() {
+    axios
+      .get('https://myflix-db14.herokuapp.com/movies')
+      .then((response) => {
+        this.setState({ movies: response.data });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }
 
+  //sets the selected movie state with value
   setSelectedMovie(newSelectedMovie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: newSelectedMovie,
+    });
+  }
+
+  //when user is verified set state to current user
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  onRegister(registered) {
+    this.setState({
+      registered,
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, registered } = this.state;
 
-    if (movies.length === 0) return <div className="main-view" />;
+    //forcing a registration form for testing
+    if (registered) {
+      return <RegistrationView onRegister={(bool) => this.onRegister(bool)} />;
+    }
 
+    //if user is no logged in - force a login form
+    if (!user) {
+      return (
+        <LoginView
+          onLoggedIn={(user) => this.onLoggedIn(user)}
+          onRegister={(bool) => this.onRegister(bool)}
+        />
+      );
+    }
+
+    if (movies.length === 0)
+      return <div className="main-view">This list is empty</div>;
+
+    //if no movie is selected show the list -
+    //if a movie is selected show the Movie View details
     return (
-      <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-        }
+      <div className="main-vew">
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(movie) => {
+                this.setSelectedMovie(movie);
+              }}
+            />
+          ))
+        )}
       </div>
     );
   }
 }
-export default MainView;
