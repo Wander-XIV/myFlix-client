8d39f4532daa931a4d2295cd31efcf1659d37f4c
Wander-XIV <aroy1420@gmail.com>
2022-06-12 11:32:57 -0400

    updated login view
    added validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,87 +1,100 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import {
-  Navbar,
-  Nav,
-  Form,
-  Button,
-  Card,
-  CardGroup,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import axios from "axios";
+
+import { Button, Col, Container, Form, Row } from "react-bootstrap/";
 
 import "./login-view.scss";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username required");
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr("Username must be at least 5 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password required");
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr("Password must be at least 6 characters long");
+      isReq = false;
+    }
+    return isReq;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    const isReq = validate();
+    if (isReq) {
+      /* Send a request to the server for authentication */
+      axios
+        .post("https://myflix-db14.herokuapp.com/login", {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log("no such user");
+        });
+    }
   };
 
   return (
-    <Container fluid className="loginContainer">
-      <Navbar fluid className="navbar">
-        <Container fluid>
-          <Navbar.Brand href="#home">myFlix</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#logout">Login</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <Card className="loginCard">
-        <Card.Body>
-          <Card.Title className="text-center">Welcome to myFlix.</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted text-center">
-            Login
-          </Card.Subtitle>
-
+    <Container id="login-form">
+      <Row className="justify-content-center">
+        <Col sm="10" md="8" lg="6">
           <Form>
             <Form.Group controlId="formUsername">
-              <Form.Label>Username</Form.Label>
+              <Form.Label>Username:</Form.Label>
               <Form.Control
                 type="text"
                 onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
               />
+              {/* display validation error */}
+              {usernameErr && <p>{usernameErr}</p>}
             </Form.Group>
-
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password</Form.Label>
+            <Form.Group controlId="formPassword" className="mt-3">
+              <Form.Label>Password:</Form.Label>
               <Form.Control
-                className="mb-3"
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
               />
+              {/* display validation error */}
+              {passwordErr && <p>{passwordErr}</p>}
             </Form.Group>
-
-            <Button
-              className="loginButton"
-              variant="secondary"
-              size="lg"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Login
-            </Button>
+            <Row className="mt-3 justify-content-start">
+              <Col sm="10" md="8" lg="6">
+                <Button variant="warning" type="submit" onClick={handleSubmit}>
+                  Login
+                </Button>
+              </Col>
+            </Row>
           </Form>
-        </Card.Body>
-      </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
 
 LoginView.propTypes = {
   user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
   }),
   onLoggedIn: PropTypes.func.isRequired,
 };
